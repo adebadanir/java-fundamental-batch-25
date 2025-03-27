948cae4b7c6c14f9f4305f11e877ea9e5345ba03
adebadanir <athbadanir@gmail.com>
2025-03-27 21:37:50 +0700

    commit pertemuan 2

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -1,7 +1,6 @@
 package com.juaracoding;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("JuaraCoding");
@@ -17,5 +16,6 @@ public class Main {
             System.out.println(i);
             System.out.println(x);
         }
+
     }
 }
